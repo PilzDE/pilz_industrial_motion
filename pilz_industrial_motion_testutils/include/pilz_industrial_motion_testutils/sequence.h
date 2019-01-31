5ce78f6923722e6148102adb91a6a56a491a899e
hsl <h.slusarek@pilz.de>
2019-01-31 14:23:49 +0100

    Add size and erase method to sequence class

--- pilz_industrial_motion_testutils/include/pilz_industrial_motion_testutils/sequence.h
+++ pilz_industrial_motion_testutils/include/pilz_industrial_motion_testutils/sequence.h
@@ -17,6 +17,7 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
 
+#include <stdexcept>
 #include <vector>
 #include <utility>
 
@@ -42,14 +43,24 @@ public:
    * @brief Adds a command to the end of the sequence.
    * @param cmd The command which has to be added.
    */
-  void add(MotionCmdUPtr cmd, double radius = 0.);
+  void add(MotionCmdUPtr cmd, const double radius = 0.);
+
+  /**
+   * @brief Returns the number of commands.
+   */
+  size_t size() const;
+
+  MotionCmd& getCmd(const size_t index_cmd);
+  const MotionCmd& getCmd(const size_t index_cmd) const;
 
   void setAllBlendRadiiToZero();
+  void setBlendRadii(const size_t index_cmd, const double radius);
+  double getBlendRadius(const size_t index_cmd) const;
 
-  MotionCmd& getCmd(size_t index_cmd);
-  const MotionCmd& getCmd(size_t index_cmd) const;
-
-  double getBlendRadius(size_t index_cmd) const;
+  /**
+   * @brief Deletes all commands from index 'start' to index 'end'.
+   */
+  void erase(const size_t start, const size_t end);
 
   pilz_msgs::MotionSequenceRequest toRequest() const;
 
@@ -57,25 +68,56 @@ private:
   std::vector<std::pair<MotionCmdUPtr, double> > cmds_;
 };
 
-inline void Sequence::add(MotionCmdUPtr cmd, double radius)
+inline void Sequence::add(MotionCmdUPtr cmd, const double radius)
 {
   assert(cmd);
   cmds_.emplace_back( std::move(cmd), radius );
 }
 
-inline MotionCmd& Sequence::getCmd(size_t index_cmd)
+inline size_t Sequence::size() const
+{
+  return cmds_.size();
+}
+
+inline MotionCmd& Sequence::getCmd(const size_t index_cmd)
 {
   return *(cmds_.at(index_cmd).first);
 }
 
-inline const MotionCmd& Sequence::getCmd(size_t index_cmd) const
+inline const MotionCmd& Sequence::getCmd(const size_t index_cmd) const
 {
   return *(cmds_.at(index_cmd).first);
 }
 
-inline double Sequence::getBlendRadius(size_t index_cmd) const
+inline double Sequence::getBlendRadius(const size_t index_cmd) const
 {
   return cmds_.at(index_cmd).second;
+}
+
+inline void Sequence::setBlendRadii(const size_t index_cmd, const double radius)
+{
+  cmds_.at(index_cmd).second = radius;
+}
+
+inline void Sequence::erase(const size_t start, const size_t end)
+{
+  const size_t orig_N {size()};
+
+  if (start > orig_N || end > orig_N)
+  {
+    std::string msg;
+    msg.append("Parameter start=").append(std::to_string(start));
+    msg.append(" and end=").append(std::to_string(end));
+    msg.append(" must not be greater then the number of #commands=");
+    msg.append(std::to_string(size()));
+    throw std::invalid_argument(msg);
+  }
+  cmds_.erase(cmds_.begin()+start, cmds_.begin()+end);
+  if (end == orig_N)
+  {
+    // Make sure last radius is set zero
+    cmds_.at(size()-1).second = 0.;
+  }
 }
 
 }
