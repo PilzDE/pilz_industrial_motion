7fb5a4ff9b4c85cf1fd118770fc7d90b4bf5c01a
hsl <h.slusarek@pilz.de>
2019-02-07 13:43:43 +0100

    Remove remaining artificts from old testloader in blending tests

--- pilz_trajectory_generation/test/integrationtest_sequence_service_capability.cpp
+++ pilz_trajectory_generation/test/integrationtest_sequence_service_capability.cpp
@@ -42,12 +42,9 @@
 #include "motion_plan_request_builder.h"
 #include "motion_sequence_request_builder.h"
 
-const std::string BLEND_DATA_PREFIX("test_data/");
-
 // Parameters from parameter server
 const std::string PARAM_PLANNING_GROUP_NAME("planning_group");
 const std::string PARAM_TARGET_LINK_NAME("target_link");
-const std::string BLEND_DATASET_NUM("blend_dataset_num");
 const std::string TEST_DATA_FILE_NAME("testdata_file_name");
 
 using namespace pilz_industrial_motion_testutils;
@@ -58,17 +55,11 @@ protected:
   virtual void SetUp();
   virtual void TearDown() {}
 
-private:
-  void generateDataSet();
-
 protected:
   ros::NodeHandle ph_ {"~"};
   ros::ServiceClient client_;
   robot_model::RobotModelPtr robot_model_;
-  std::vector<testutils::blend_test_data> test_data_;
   std::string planning_group_, target_link_;
-
-  pilz_msgs::MotionSequenceRequest blend_command_list_2_, blend_command_list_3_;
 
   std::string test_data_file_name_;
   TestdataLoaderUPtr data_loader_;
@@ -81,79 +72,18 @@ void IntegrationTestSequenceService::SetUp()
   ASSERT_TRUE(ph_.getParam(PARAM_TARGET_LINK_NAME, target_link_));
   ASSERT_TRUE(ph_.getParam(TEST_DATA_FILE_NAME, test_data_file_name_));
 
-  // create robot model
+  // create robot model + check model
   robot_model_loader::RobotModelLoader model_loader;
   robot_model_ = model_loader.getModel();
-
-  // check robot model
   testutils::checkRobotModel(robot_model_, planning_group_, target_link_);
 
   // load the test data provider
   data_loader_.reset(new XmlTestdataLoader(test_data_file_name_, robot_model_));
   ASSERT_NE(nullptr, data_loader_) << "Failed to load test data by provider.";
 
-  // get test data set
-  int blend_dataset_num;
-  ASSERT_TRUE(ph_.getParam(BLEND_DATASET_NUM, blend_dataset_num));
-  ASSERT_TRUE(testutils::getBlendTestData(ph_, blend_dataset_num, BLEND_DATA_PREFIX, test_data_));
-
-  generateDataSet();
-
   ASSERT_TRUE(ros::service::waitForService(pilz_trajectory_generation::SEQUENCE_SERVICE_NAME, ros::Duration(10))) << "Service not available.";
   ros::NodeHandle nh; // connect to service in global namespace, not in ph_
   client_ = nh.serviceClient<pilz_msgs::GetMotionSequence>(pilz_trajectory_generation::SEQUENCE_SERVICE_NAME);
-}
-
-void IntegrationTestSequenceService::generateDataSet()
-{
-  geometry_msgs::PoseStamped p1, p2, p3;
-
-  p1.header.frame_id = robot_model_->getModelFrame();
-  p1.pose.position.x = 0.25;
-  p1.pose.position.y = 0.3;
-  p1.pose.position.z = 0.65;
-  p1.pose.orientation.x = 0.;
-  p1.pose.orientation.y = 0.;
-  p1.pose.orientation.z = 0.;
-  p1.pose.orientation.w = 1.;
-
-  p2 = p1;
-  p2.pose.position.x -= 0.15;
-
-  // TODO: move p3 to testUtils
-  p3 = p1;
-  p3.pose.position.y -= 0.15;
-
-  MotionPlanRequestBuilder builder("manipulator");
-
-  // goal 1 (with start state)
-  sensor_msgs::JointState start_state_joint = testutils::generateJointState({0., 0.007881892504574495, -1.8157263253868452,
-                                                                             0., 1.8236082178909834, 0.});
-  builder.setJointStartState(start_state_joint);
-  builder.setGoal(target_link_, p1);
-
-  moveit_msgs::MotionPlanRequest  req1, req2, req3;
-
-  req1 = builder.createLin();
-
-  // Clear the start state for the next goals
-  builder.clearJointStartState();
-
-  // goal 2
-  builder.setGoal(target_link_, p2);
-  req2 = builder.createLin();
-
-  // goal3
-  builder.setGoal(target_link_, p3);
-  req3 = builder.createLin();
-
-  MotionSequenceRequestBuilder blend_list_builder;
-  blend_command_list_2_ = blend_list_builder.build({std::make_pair(req1, 0.08),
-                                                    std::make_pair(req2, 0)});
-
-  blend_command_list_3_ = blend_list_builder.build({std::make_pair(req1, 0.08),
-                                                    std::make_pair(req2, 0.05),
-                                                    std::make_pair(req3, 0)});
 }
 
 /**
@@ -293,16 +223,17 @@ TEST_F(IntegrationTestSequenceService, TestTooLargeBlendRadii)
  * start state is sent.
  *
  *  Test Sequence:
- *    1. Generate request, second goal has an invalid start state set +  Call sequence service.
+ *    1. Generate request (second goal has invalid start state) +  Call sequence service.
  *    2. Evaluate the result
  *
  *  Expected Results:
  *    1. MotionPlanResponse is received.
  *    2. Command fails, result trajectory is empty.
  */
-TEST_F(IntegrationTestSequenceService, TestSecondStartStateNotFirstGoal)
+TEST_F(IntegrationTestSequenceService, TestSecondTrajInvalidStartState)
 {
-  pilz_msgs::MotionSequenceRequest req_list = blend_command_list_2_;
+  Sequence seq {data_loader_->getSequence("ComplexSequence")};
+  pilz_msgs::MotionSequenceRequest req_list {seq.toRequest()};
   req_list.items[1].req.start_state.joint_state = testutils::generateJointState({-1., 2., -3., 4., -5., 0.});
 
   pilz_msgs::GetMotionSequence srv;
@@ -329,7 +260,8 @@ TEST_F(IntegrationTestSequenceService, TestSecondStartStateNotFirstGoal)
  */
 TEST_F(IntegrationTestSequenceService, TestFirstGoalNotReachable)
 {
-  pilz_msgs::MotionSequenceRequest req_list = blend_command_list_2_;
+  Sequence seq {data_loader_->getSequence("ComplexSequence")};
+  pilz_msgs::MotionSequenceRequest req_list {seq.toRequest()};
   req_list.items[0].req.goal_constraints[0].position_constraints[0].constraint_region.primitive_poses[0].position.y = 27;
 
   pilz_msgs::GetMotionSequence srv;
@@ -372,37 +304,6 @@ TEST_F(IntegrationTestSequenceService, TestInvalidLinkName)
   const moveit_msgs::MotionPlanResponse& response {srv.response.plan_response};
   EXPECT_NE(moveit_msgs::MoveItErrorCodes::SUCCESS, response.error_code.val) << "Incorrect error code.";
   EXPECT_EQ(0u, response.trajectory.joint_trajectory.points.size()) << "Planned trajectory not empty.";
-}
-
-/**
- * @brief Tests the LIN-LIN blending.
- *
- * Test Sequence:
- *    1. Call service.
- *    2. Evaluate the result.
- *
- * Expected Results:
- *    1. MotionPlanResponse is received.
- *    2. Error code of the result is success.
- */
-TEST_F(IntegrationTestSequenceService, TestLinLinBlending)
-{
-  for(const auto& test_data : test_data_)
-  {
-    pilz_msgs::MotionSequenceRequest req_list;
-    testutils::generateRequestMsgFromBlendTestData(robot_model_, test_data,
-                                                   "LIN", planning_group_,
-                                                   target_link_, req_list);
-
-    pilz_msgs::GetMotionSequence srv;
-    srv.request.commands = req_list;
-
-    ASSERT_TRUE(client_.call(srv));
-
-    const moveit_msgs::MotionPlanResponse& response {srv.response.plan_response};
-    EXPECT_EQ(moveit_msgs::MoveItErrorCodes::SUCCESS, response.error_code.val) << "Planning of blend trajectory failed!";
-    EXPECT_GT(response.trajectory.joint_trajectory.points.size(), 0u) << "Trajectory should contain points.";
-  }
 }
 
 /**
@@ -498,7 +399,6 @@ TEST_F(IntegrationTestSequenceService, TestComplexSequenceWithBlending)
   const moveit_msgs::MotionPlanResponse& response {srv.response.plan_response};
   EXPECT_EQ(moveit_msgs::MoveItErrorCodes::SUCCESS, response.error_code.val) << "Incorrect error code.";
   EXPECT_GT(response.trajectory.joint_trajectory.points.size(), 0u) << "Trajectory should contain points.";
-
 }
 
 int main(int argc, char **argv)
