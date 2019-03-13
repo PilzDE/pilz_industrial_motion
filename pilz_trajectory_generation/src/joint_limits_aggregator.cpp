fd9a8f9db451b254e8186c3b0e629345589a1da2
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2019-03-13 14:16:37 +0100

    Fix wrong coverage exclude in joint_limits_aggregator

--- pilz_trajectory_generation/src/joint_limits_aggregator.cpp
+++ pilz_trajectory_generation/src/joint_limits_aggregator.cpp
@@ -87,24 +87,24 @@ void pilz::JointLimitsAggregator::updatePositionLimitFromJointModel(const moveit
 {
   switch(joint_model->getVariableBounds().size())
   {
-  case 0:
-    // LCOV_EXCL_START
+  // LCOV_EXCL_START
+  case 0:  
     ROS_ERROR_STREAM("no bounds set for joint " << joint_model->getName());
     break;
-    // LCOV_EXCL_STOP
+  // LCOV_EXCL_STOP
   case 1:
     joint_limit.has_position_limits = joint_model->getVariableBounds()[0].position_bounded_;
     joint_limit.min_position = joint_model->getVariableBounds()[0].min_position_;
     joint_limit.max_position = joint_model->getVariableBounds()[0].max_position_;
     break;
+  // LCOV_EXCL_START
   default:
-    // LCOV_EXCL_START
     ROS_ERROR_STREAM("Multi-DOF-Joints not supported. The robot won't move.");
     joint_limit.has_position_limits = true;
     joint_limit.min_position = 0;
     joint_limit.max_position = 0;
     break;
-    // LCOV_EXCL_STOP
+  // LCOV_EXCL_STOP
   }
 
   ROS_DEBUG_STREAM("Limit(" << joint_model->getName() << " min:" << joint_limit.min_position << " max:" << joint_limit.max_position);
@@ -115,22 +115,22 @@ void pilz::JointLimitsAggregator::updateVelocityLimitFromJointModel(const moveit
 {
   switch(joint_model->getVariableBounds().size())
   {
+  // LCOV_EXCL_START
   case 0:
-    // LCOV_EXCL_START
     ROS_ERROR_STREAM("no bounds set for joint " << joint_model->getName());
     break;
-    // LCOV_EXCL_STOP
+  // LCOV_EXCL_STOP
   case 1:
     joint_limit.has_velocity_limits = joint_model->getVariableBounds()[0].velocity_bounded_;
     joint_limit.max_velocity = joint_model->getVariableBounds()[0].max_velocity_;
     break;
+  // LCOV_EXCL_START
   default:
-    // LCOV_EXCL_START
     ROS_ERROR_STREAM("Multi-DOF-Joints not supported. The robot won't move.");
     joint_limit.has_velocity_limits = true;
     joint_limit.max_velocity = 0;
     break;
-    // LCOV_EXCL_STOP
+  // LCOV_EXCL_STOP
   }
 }
 
