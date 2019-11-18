c0ba84418625e9ff60a601e2a893fe6e99307859
Immanuel Martini <immanuel.martini@gmail.com>
2019-11-18 15:00:10 +0100

    minor improvements

--- .coverage.sh
+++ .coverage.sh
@@ -11,21 +11,22 @@ for pkg in $COVERAGE_PKGS; do
     ws=~/target_ws
     extend="/opt/ros/$ROS_DISTRO"
     ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage
-    cd $TARGET_REPO_PATH
 
     if [ -a $ws/build/$pkg/${pkg}_coverage.info.cleaned ]; then
         echo "Coverage summary for $pkg ----------------------"
         lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned
         echo "---------------------------------------------------"
 
-        line_cov_percentage=$(lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned 2>&1 | grep -Poi "lines\.*: \K[0-9.]*")
+        line_cov_percentage=$(lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned 2>&1
+                              | grep -Poi "lines\.*: \K[0-9.]*")
         required_coverage="100.0"
     else
         cd $HOME/.ros
-        echo "Coverage summary for $pkg ----------------------"
-        python-coverage report --include "*$ws/src/$TARGET_REPO_NAME/$pkg*" --omit "*/$pkg/test/*"
-        echo "---------------------------------------------------"
-        line_cov_percentage=$(python-coverage report --include "$ws/src/$TARGET_REPO_NAME/$pkg*" --omit "*/$pkg/test/*" | grep -Poi "TOTAL.* ([0-9]*){2} \K[0-9]*")
+        echo "Coverage summary for $pkg"
+        python-coverage report --include "*$TARGET_REPO_PATH/$pkg*" --omit "*/$pkg/test/*"
+
+        line_cov_percentage=$(python-coverage report --include "$TARGET_REPO_PATH/$pkg*" --omit "*/$pkg/test/*"
+                              | grep -Poi "TOTAL.* ([0-9]*){2} \K[0-9]*")
         required_coverage="100"
     fi
 
