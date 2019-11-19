d9cb1cd5144f96197f87d04a254e3840cc40ad85
leunMar <36481640+leunMar@users.noreply.github.com>
2019-11-19 14:48:23 +0100

    Add support for Python coverage CI (#227)
    * Support python-coverage reports in CI script
    * Add pilz_robot_programming to coverage job

--- .coverage.sh
+++ .coverage.sh
@@ -11,13 +11,22 @@ for pkg in $COVERAGE_PKGS; do
     ws=~/target_ws
     extend="/opt/ros/$ROS_DISTRO"
     ici_exec_in_workspace "$extend" "$ws" catkin build $pkg -v --no-deps --catkin-make-args ${pkg}_coverage
-    cd $TARGET_REPO_PATH
-    echo "Coverage summary for $pkg ----------------------"
-    lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned
-    echo "---------------------------------------------------"
 
-    line_cov_percentage=$(lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned 2>&1 | grep -Poi "lines\.*: \K[0-9.]*")
-    required_coverage="100.0"
+    if [ -a $ws/build/$pkg/${pkg}_coverage.info.cleaned ]; then
+        echo "Coverage summary for $pkg ----------------------"
+        lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned
+        echo "---------------------------------------------------"
+
+        line_cov_percentage=$(lcov --summary $ws/build/$pkg/${pkg}_coverage.info.cleaned 2>&1 | grep -Poi "lines\.*: \K[0-9.]*")
+        required_coverage="100.0"
+    else
+        cd $HOME/.ros
+        echo "Coverage summary for $pkg"
+        python-coverage report --include "$ws/src/$TARGET_REPO_NAME/$pkg/*" --omit "*/$pkg/test/*"
+
+        line_cov_percentage=$(python-coverage report --include "$ws/src/$TARGET_REPO_NAME/$pkg/*" --omit "*/$pkg/test/*" | grep -Poi "TOTAL.* ([0-9]*){2} \K[0-9]*")
+        required_coverage="100"
+    fi
 
     if [ "$line_cov_percentage" != "$required_coverage" ]; then
         result_str="$pkg: $line_cov_percentage%(required:$required_coverage%)\e[${ANSI_RED}m[failed]\e[0m"
