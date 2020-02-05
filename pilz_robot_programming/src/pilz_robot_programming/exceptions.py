ba4a253434aa99a8d5b417bb0f8f47d705d9ce76
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2020-02-05 18:36:01 +0100

    Remove unused import of prbt_hardware_support
    * Closes #240

--- pilz_robot_programming/src/pilz_robot_programming/exceptions.py
+++ pilz_robot_programming/src/pilz_robot_programming/exceptions.py
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from prbt_hardware_support.msg import BrakeTestErrorCodes
-
 
 class RobotVersionError(Exception):
     pass
