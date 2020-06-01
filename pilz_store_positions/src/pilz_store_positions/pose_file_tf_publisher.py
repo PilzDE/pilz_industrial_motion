6b22a1064c8266c15011f4cb2ca97280bbfcf60e
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2020-06-01 20:53:46 +0200

    Fix license headers

--- pilz_store_positions/src/pilz_store_positions/pose_file_tf_publisher.py
+++ pilz_store_positions/src/pilz_store_positions/pose_file_tf_publisher.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Pilz GmbH & Co. KG
+# Copyright (c) 2020 Pilz GmbH & Co. KG
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
